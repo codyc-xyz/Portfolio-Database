1ed9a39f2c286de701d4b58bf1f7c13abb668a3b
Red <crabmarket56@gmail.com>
2023-03-19 15:28:12 +0900

    Add books

--- books.sql
+++ books.sql
@@ -19,3 +19,12 @@ VALUES ('The Concept of Irony, with Continual Reference to Socrates', DATE '1841
 
 INSERT INTO book (title, date_published, pages, genres, isbn, cover_image, goodreads_link, author_uid)
 VALUES ('Either/Or Part I', DATE '1843-02-20', 466, '{Existentialism,"Philosophy of Love",Aesthetics}', '978-0691020419', 'https://i5.walmartimages.com/asr/77d71d35-2683-43c3-866e-9e7daa9cc2ce_1.3b981c38314a6fbb954e4eb59592fb89.jpeg', 'https://www.goodreads.com/book/show/24971.Either_Or_Part_I', '072dfd0a-3638-4028-8993-d10dc67f2801');
+
+INSERT INTO book (title, date_published, pages, genres, isbn, cover_image, goodreads_link, author_uid)
+VALUES ('Either/Or Part II', DATE '1843-02-20', 373, '{Existentialism,Ethics,"Philosophy of Religion"}', '978-0691020426', 'https://pictures.abebooks.com/isbn/9780691020426-us.jpg', 'https://www.goodreads.com/book/show/862964.Either_Or_Part_II', '072dfd0a-3638-4028-8993-d10dc67f2801');
+
+INSERT INTO book (title, date_published, pages, genres, isbn, cover_image, goodreads_link, author_uid)
+VALUES ('The Concept of Anxiety: A Simple Psychologically Oriented Deliberation in View of the Dogmatic Problem of Hereditary Sin', DATE '1844-06-17', 181, '{Existentialism,Psychology,"Philosophy of Religion"}', '978-1631490040', 'https://d202m5krfqbpi5.cloudfront.net/books/1171039662l/83325.jpg', 'https://www.goodreads.com/book/show/17987667-the-concept-of-anxiety', '072dfd0a-3638-4028-8993-d10dc67f2801');
+
+INSERT INTO book (title, date_published, pages, genres, isbn, cover_image, goodreads_link, author_uid)
+VALUES ('The Myth of Sisypyhus and Other Essays', DATE '1942-01-01', 165, '{Absurdism,Existentialism}', '0-679-73373-6', 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1411534599l/870193.jpg', 'https://www.goodreads.com/book/show/91950.The_Myth_of_Sisyphus', '92a4b579-8eb9-4c8a-bb21-78e34a752b5a');

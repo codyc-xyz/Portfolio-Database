d6dee44d4ea9bd3cf5f6624d8550b3a480fbbbca
Red <crabmarket56@gmail.com>
2023-03-19 17:06:00 +0900

    Add quotes

--- quotes.sql
+++ quotes.sql
@@ -30,3 +30,17 @@ VALUES ('This heart within me I can feel, and I judge that it exists. This world
 
 INSERT INTO quote (text, page_number, book_uid, chapter)
 VALUES ('This very heart which is mine will forever remain indefinable for me. Between the certainty I have of my existence and the content I try to give to that assurance, the gap will never be filled. Forever I shall be a stranger to myself. In psychology as in logic, there are truths but no truth.', 18, '0ace69d6-f36c-41bf-9189-6f833833e157', 'Absurd Walls'),
+
+INSERT INTO quote (text, page_number, book_uid, chapter)
+VALUES ('Unlike Eurydice, the absurd dies only when we turn away from it. One of the only coherent philosophical positions is thus revolt. It is a constant confrontation between man and his own obscurity. It is an insistence upon an impossible transparency. It challenges the world anew every second. Just as danger provided man the unique opportunity of seizing awareness, so metaphysical revolt extends awareness to the whole of experience. It is that constant presence of man in his own eyes. It is not aspiration, for it is devoid of hope. That revolt is the certainty of a crushing fate, without the resignation that ought to accompany it.', 42, '0ace69d6-f36c-41bf-9189-6f833833e157', 'Absurd Freedom');
+
+INSERT INTO quote (text, page_number, book_uid, chapter)
+VALUES('The absurd man can only drain everything to the bitter end, and deplete himself. The absurd is his extreme tension, which he maintains constantly by solitary effort, for he knows that in that consciousness and in that day-to-day revolt he gives proof of his only truth, which is defiance.', 43, '0ace69d6-f36c-41bf-9189-6f833833e157', 'Absurd Freedom');
+
+INSERT INTO quote (text, page_number, book_uid, chapter)
+VALUES('the absurd man realizes that hitherto he was bound to that postulate of freedom on the illusion of which he was living. In a certain sense, that hampered him. To the extent to which he imagined a purpose to his life, he adapted himself to the demands of a purpose to be achieved and became the slave of his liberty.', 45, '0ace69d6-f36c-41bf-9189-6f833833e157', 'Absurd Freedom');
+
+
+
+
+

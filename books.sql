56a96206c6e418648f528420cbade8fc9d7bf471
Red <crabmarket56@gmail.com>
2023-03-26 22:51:49 +0900

    Add excerpts and fix book page numbers

--- books.sql
+++ books.sql
@@ -18,10 +18,10 @@ INSERT INTO book (title, date_published, pages, genres, isbn, cover_image, goodr
 VALUES ('The Concept of Irony, with Continual Reference to Socrates', DATE '1841-09-29', 340, '{"Philosophy of Language","History of Philosophy"}', '978-0-691-02072-3', 'https://pictures.abebooks.com/inventory/md/md30709059979.jpg', 'https://www.goodreads.com/book/show/99290.The_Concept_of_Irony', '072dfd0a-3638-4028-8993-d10dc67f2801');
 
 INSERT INTO book (title, date_published, pages, genres, isbn, cover_image, goodreads_link, author_uid)
-VALUES ('Either/Or Part I', DATE '1843-02-20', 466, '{Existentialism,"Philosophy of Love",Aesthetics}', '978-0691020419', 'https://i5.walmartimages.com/asr/77d71d35-2683-43c3-866e-9e7daa9cc2ce_1.3b981c38314a6fbb954e4eb59592fb89.jpeg', 'https://www.goodreads.com/book/show/24971.Either_Or_Part_I', '072dfd0a-3638-4028-8993-d10dc67f2801');
+VALUES ('Either/Or Part I', DATE '1843-02-20', 445, '{Existentialism,"Philosophy of Love",Aesthetics}', '978-0691020419', 'https://i5.walmartimages.com/asr/77d71d35-2683-43c3-866e-9e7daa9cc2ce_1.3b981c38314a6fbb954e4eb59592fb89.jpeg', 'https://www.goodreads.com/book/show/24971.Either_Or_Part_I', '072dfd0a-3638-4028-8993-d10dc67f2801');
 
 INSERT INTO book (title, date_published, pages, genres, isbn, cover_image, goodreads_link, author_uid)
-VALUES ('Either/Or Part II', DATE '1843-02-20', 373, '{Existentialism,Ethics,"Philosophy of Religion"}', '978-0691020426', 'https://pictures.abebooks.com/isbn/9780691020426-us.jpg', 'https://www.goodreads.com/book/show/862964.Either_Or_Part_II', '072dfd0a-3638-4028-8993-d10dc67f2801');
+VALUES ('Either/Or Part II', DATE '1843-02-20', 338, '{Existentialism,Ethics,"Philosophy of Religion"}', '978-0691020426', 'https://pictures.abebooks.com/isbn/9780691020426-us.jpg', 'https://www.goodreads.com/book/show/862964.Either_Or_Part_II', '072dfd0a-3638-4028-8993-d10dc67f2801');
 
 INSERT INTO book (title, date_published, pages, genres, isbn, cover_image, goodreads_link, author_uid)
 VALUES ('The Concept of Anxiety: A Simple Psychologically Oriented Deliberation in View of the Dogmatic Problem of Hereditary Sin', DATE '1844-06-17', 181, '{Existentialism,Psychology,"Philosophy of Religion"}', '978-1631490040', 'https://d202m5krfqbpi5.cloudfront.net/books/1171039662l/83325.jpg', 'https://www.goodreads.com/book/show/17987667-the-concept-of-anxiety', '072dfd0a-3638-4028-8993-d10dc67f2801');

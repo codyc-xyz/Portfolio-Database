f2d3d083edf35dac6c30158ec502df941129d274
Red <crabmarket56@gmail.com>
2023-03-27 15:31:40 +0900

    Update cover image links

--- books.sql
+++ books.sql
@@ -20,7 +20,7 @@ DATE '1843-10-16',
 95, 
 '{Existentialism,"Philosophy of Religion"}', 
 '978-0140444490', 
-'https://i.pinimg.com/736x/9c/a8/33/9ca83312a2bfb9d7d8efb472ff07a02c.jpg', 
+'https://i.postimg.cc/66mpys0Y/fear-and-trembling.jpg', 
 'https://www.goodreads.com/book/show/24965.Fear_and_Trembling', 
 '072dfd0a-3638-4028-8993-d10dc67f2801');
 
@@ -44,7 +44,7 @@ DATE '1843-02-20',
 445, 
 '{Existentialism,"Philosophy of Love",Aesthetics}', 
 '978-0691020419', 
-'https://i5.walmartimages.com/asr/77d71d35-2683-43c3-866e-9e7daa9cc2ce_1.3b981c38314a6fbb954e4eb59592fb89.jpeg', 
+'https://i.postimg.cc/1X2mT6tL/either-or-part-I.jpg', 
 'https://www.goodreads.com/book/show/24971.Either_Or_Part_I', 
 '072dfd0a-3638-4028-8993-d10dc67f2801');
 
@@ -56,7 +56,7 @@ DATE '1843-02-20',
 338, 
 '{Existentialism,Ethics,"Philosophy of Religion"}', 
 '978-0691020426', 
-'https://pictures.abebooks.com/isbn/9780691020426-us.jpg', 
+'https://i.postimg.cc/VkXBLWjL/either-or-part-II.jpg', 
 'https://www.goodreads.com/book/show/862964.Either_Or_Part_II', 
 '072dfd0a-3638-4028-8993-d10dc67f2801');
 
@@ -68,7 +68,7 @@ DATE '1844-06-17',
 181, 
 '{Existentialism,Psychology,"Philosophy of Religion"}', 
 '978-1631490040', 
-'https://d202m5krfqbpi5.cloudfront.net/books/1171039662l/83325.jpg', 
+'https://i.postimg.cc/4356p4LD/the-concept-of-anxiety.jpg', 
 'https://www.goodreads.com/book/show/17987667-the-concept-of-anxiety', 
 '072dfd0a-3638-4028-8993-d10dc67f2801');
 
@@ -80,7 +80,7 @@ DATE '1942-01-01',
 152, 
 '{Absurdism,Existentialism}', 
 '0-679-73373-6', 
-'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1411534599l/870193.jpg', 
+'https://i.postimg.cc/1t3pBxbf/myth-of-sisyphus.jpg', 
 'https://www.goodreads.com/book/show/91950.The_Myth_of_Sisyphus', 
 '92a4b579-8eb9-4c8a-bb21-78e34a752b5a');
 

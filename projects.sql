24c2ed9abce3f71df3a751d69b32c81fe1756a51
Red <crabmarket56@gmail.com>
2023-06-14 11:12:01 +0900

    remove less relevant tech from projects

--- projects.sql
+++ projects.sql
@@ -9,10 +9,10 @@ CREATE TABLE projects (
   project_image VARCHAR(255) NOT NULL,
   project_link VARCHAR(255),
   github_project_link VARCHAR(255) NOT NULL,
-  github_ui_link VARCHAR(255);
+  github_ui_link VARCHAR(255)
 );
 
-INSERT INTO projects(project_name, project_status, project_description, technologies, date_started, project_image, project_link, github_project_link, github_ui_link)
+INSERT INTO projects(project_name, project_status, project_description, technologies, date_started, project_size, project_image, project_link, github_project_link, github_ui_link)
 VALUES('Personal Website',
 'Active',
 'Welcome to my personal website, an interactive and dynamic portfolio designed with Figma and built with Typescript, React, Gatsby, Postgres, GraphQL, Node, and Tailwind. This site encapsulates my technical proficiency, passion for content curation, and unique user interface design.
@@ -26,19 +26,19 @@ DATE '2023-05-11',
 'https://i.postimg.cc/y82Rwpsh/website-homepage-adjusted.jpg',
 'http://localhost:8000/',
 'https://github.com/0xdacted/portfolio_site',
-NULL,
+NULL
 );
 
-INSERT INTO projects(project_name, project_status, project_description, technologies, date_started, project_image, project_link, github_project_link, github_ui_link)
+INSERT INTO projects(project_name,  project_status, project_description, technologies, date_started, project_size, project_image, project_link, github_project_link, github_ui_link)
 VALUES('Twitter Scraper & Over/Under Game',
+'Completed',
 'This project consists of two interconnected components: a Twitter data scraper and an Over/Under game. The project aims to provide an engaging way for users to guess and learn about the popularity of various hashtags on Twitter.
 The Twitter Data Scraper is a Python application that uses the Tweepy library to fetch and analyze real-time tweet data. Utilizing Twitter''s API, this application streams tweets, extracts all hashtags from the tweet text, and stores the data in a CSV file. The user can choose to stream tweets, analyze tweets, view the top 1000 hashtags, or play the Over/Under game by entering the appropriate command when prompted.
 The Over/Under Game is a React application that presents the user with a game based on the data scraped by the Twitter Data Scraper. The game randomly selects two hashtags from the dataset and asks the user to guess which one was tweeted more frequently during a specific time period.
 The game starts when the user clicks the "Play" button, and ends either when the user makes a wrong guess or when all hashtags have been used. The user''s score is incremented by one for every correct guess, and the high score is updated if the current score exceeds it. After each round, a new pair of hashtags is presented for the next guess.
 This combination of data scraper and game provides an engaging, interactive way for users to explore Twitter trends and test their intuition about the popularity of various topics on social media.
 Please note that this project is based on publicly available Twitter data and the game does not filter out potentially inappropriate content in the hashtags.',
-'Completed',
-'{Typescript, React, Python, Pandas, Tweepy, Tailwind}',
+'{Typescript, React, Python, Pandas, Tailwind}',
 DATE '2023-01-25',
 'Medium',
 'https://i.postimg.cc/c49wc4zb/over-under-game-adjusted.jpg',
@@ -47,14 +47,14 @@ DATE '2023-01-25',
 'https://github.com/0xdacted/portfolio_site/blob/main/src/pages/over_under.tsx'
 );
 
-INSERT INTO projects(project_name, project_status, project_description, technologies, date_started, project_image, project_link, github_project_link, github_ui_link)
+INSERT INTO projects(project_name, project_status, project_description, technologies, date_started, project_size, project_image, project_link, github_project_link, github_ui_link)
 VALUES('Image Resizer',
+'Completed',
 'Image Resizer is a user-friendly application designed to make image resizing a breeze for the end-users. Built using a React frontend and an Express backend, this application supports a variety of image formats, including JPEG, PNG, and GIF. It offers a dual-mode of image upload - from the local system and via direct URL links, providing flexibility to the users. Users can upload multiple images simultaneously, but to maintain application performance, a cap on the number of images that can be uploaded at a time is implemented.
 Once the images are uploaded, users can specify their desired dimensions in terms of width and height. The application has a built-in validation mechanism to ensure that these dimensions are positive numbers before resizing the images. It also validates the uploaded images to ensure they are in acceptable formats.
 The resized images are made available for download, and the application efficiently handles the temporary storage of these images, deleting them automatically after download. Robust error handling mechanisms are in place to ensure that any issues during the image upload, validation, or resizing process are clearly communicated to the user.
 A distinctive feature of this application is the reset functionality, which allows users to conveniently clear out all the uploaded images, links, and specified dimensions, resetting the application''s state.',
-'Completed',
-'{Typescript, React, Tailwind, REST, Node, Multer}',
+'{Typescript, React, Tailwind, REST, Node}',
 DATE '2023-06-07',
 'Small',
 'https://i.postimg.cc/bNN1b2mn/image-resizer-adjusted.jpg',

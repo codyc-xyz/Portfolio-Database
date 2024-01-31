76fbcd21be7e80b50d586e8c0f495ad1e19dc496
Red <crabmarket56@gmail.com>
2024-01-31 18:21:59 -0500

    Add Workout Tracker app

--- projects.sql
+++ projects.sql
@@ -66,15 +66,32 @@ DATE '2023-06-07',
 INSERT INTO projects(project_name, project_status, project_description, technologies, date_started, project_size, project_image, project_link, github_project_link, github_ui_link)
 VALUES('Team Selkirk App',
 'Completed',
-'As the solo developer of the Team Selkirk App, I successfully blended advanced technology with a passion for sports community engagement. Utilizing Flutter for the front-end and Firebase for the backend, this exclusive app was created for the members of Team Selkirk, part of Selkirk Sport''s Pickleball community. Available on both the Apple App Store and Google Playstore, the app is a hub for special events, promotions, initiatives, and educational courses, tailor-made for the pickleball enthusiasts of Team Selkirk.
-The app features a comprehensive backend with over 20 Firebase functions. These functions encompass a variety of tasks such as user management, adding events and promotions, distributing push notifications, and collecting, analyzing and updating user progress via both in-app and out-of-app channels. This backend functionality is pivotal in tracking user progress, generating QR codes for easy event access, and updating content in real-time.
+'As the solo developer of the Team Selkirk App, I successfully blended technology with a passion for sports community engagement. Utilizing Flutter for the front-end and Firebase for the backend, this exclusive app was created for the members of Team Selkirk, part of Selkirk Sport''s Pickleball community. Available on both the Apple App Store and Google Playstore, the app is a hub for special events, promotions, initiatives, and educational courses, tailor-made for the pickleball enthusiasts of Team Selkirk.
+The app features two-factor auth, approaching pixel perfect implementation of Figma wireframes and a comprehensive backend with over 20 Firebase functions. These Firebase functions encompass a variety of tasks such as user management, adding events and promotions, distributing push notifications, and collecting, analyzing and updating user progress via both in-app and out-of-app channels. This backend functionality is pivotal in tracking user progress, generating QR codes for easy event access, and updating content in real-time.
 A key feature of my development work was the creation of a user-friendly GUI for the backend. This interface allows non-technical staff to easily add and manage content, ensuring that the app remains dynamic and up-to-date with the latest community offerings.
 Throughout my tenure at Selkirk, the Team Selkirk App saw over 1000 active users and efficiently distributed over $25,000 in completion-based promotions. This level of engagement not only demonstrates the app''s popularity but also its effectiveness in fostering a connected and active community within Selkirk Sport''s Pickleball circle.',
-'{Flutter, Firebase, Figma, IOS, Android}',
+'{Flutter, Firebase, Figma, IOS, Android, Javascript}',
 DATE '2023-08-01',
 'Large',
 'https://i.postimg.cc/N0n3p1zz/square-logo.png',
 'https://apps.apple.com/us/app/team-selkirk/id6463821595',
+NULL,
+NULL
+);
+
+INSERT INTO projects(project_name, project_status, project_description, technologies, date_started, project_size, project_image, project_link, github_project_link, github_ui_link)
+VALUES('Workout Tracker App',
+'Completed',
+`The Workout Tracker App is a dynamic and user-friendly application designed to assist fitness enthusiasts in tracking and managing their workout routines. Developed using TypeScript and React Native, this application offers a robust and responsive user experience, backed by a SQLite database to ensure efficient data handling and storage.
+The application boasts three primary views, each meticulously crafted to cater to the diverse needs of its users.
+The centerpiece of the app, the Home View, presents users with a weekly overview of their custom workout routines, spanning Monday to Sunday. Users can input detailed information for each workout, including the name, sets, and repetitions. The interface features an intuitive design with cards for each workout, displaying bubbles corresponding to each set and the designated number of reps. Users can interact with these bubbles to log their completed reps, offering a real-time tracking experience. The progress is visually represented and maintained throughout the current week, with completed day's icons turning green, providing a gratifying sense of achievement.
+The History view offers a comprehensive calendar display, enabling users to delve into their workout history. By selecting any date on the calendar, users can view the specifics of the workouts completed on that particular day, including the number of repetitions for each set. Days marked with completed workouts are easily identifiable by a green dot. This permanent historical record allows users to scroll through their entire workout journey since their first use of the app, offering valuable insights into their fitness progress over time.
+The Progress view showcases each distinct workout the user has ever completed, including details like the name, sets, and reps. A key feature is the line chart that plots the entire workout history for each exercise, from the initial session to the most recent one. The chart transitions seamlessly between metric and imperial units, depending on the unit used in the user's last workout. The chart's X-axis represents the date, while the Y-axis denotes the weight, offering a visual representation of the user's strength and endurance growth.`,
+'{Typescript, React Native, SQLite, IOS, Android, SQL}',
+DATE '2024-01-18',
+'Medium',
+'https://i.postimg.cc/DyGLCgQD/fitness-tracker.webp',
+'https://vimeo.com/908391773?share=copy',
 NULL,
 NULL
 );
